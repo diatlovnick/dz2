92179bac270fd961a3cd52b5ca88cd4b44bbf5fe
Nickolai <datlovnick@mail.ru>
2022-05-16 19:13:37 +0300

    сделал вторую задачу

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.WriteLine("введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+string str = number.ToString();// не до конца понимаю как это работает
+int length = str.Length; // на лекции так показывали как проверять длину массива, надеюсь так тоже можно использовать
+Console.WriteLine(length);
+if (length > 2)
+{
+    Console.WriteLine($"{number} -> {str[2]}");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
